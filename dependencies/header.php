5536d27987d35bdefd8cca4b678e95563acae996
jbarkun <jbarkun@gmail.com>
2015-10-29 16:27:03 -0400

    2015/10/29 v4
    Put fonts back

--- dependencies/header.php
+++ dependencies/header.php
@@ -29,6 +29,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
 <!-- Fonts & Icons -->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 <!--Customized Style Sheet-->
